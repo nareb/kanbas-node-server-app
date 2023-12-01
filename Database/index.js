16d6427d95280982f61081b2258a7e7c9dd23779
shakyab <shakya.b@northeastern.edu>
2023-12-01 11:32:28 -0500

    Updated Node

--- Database/index.js
+++ Database/index.js
@@ -4,6 +4,7 @@ import assignments from "./assignments.json" assert { type: "json" };
 import users from "./users.json" assert { type: "json" };
 import grades from "./grades.json" assert { type: "json" };
 import enrollments from "./enrollments.json" assert { type: "json" };
+
 export default {
   courses,
   modules,
