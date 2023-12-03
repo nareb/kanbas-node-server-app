9a8c37f48d98aa71e89bfa0d8398cc8b9987a5e6
shakyab <shakya.b@northeastern.edu>
2023-12-02 22:23:34 -0500

    Fixing Heroku/Render deploy issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import HelloRoutes from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
