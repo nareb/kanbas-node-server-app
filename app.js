775a4161ee009990786f1f6d44e445c19366d95a
shakyab <shakya.b@northeastern.edu>
2023-12-04 13:34:37 -0500

    Updated Mongodb atlas link

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ import FollowsRoutes from "./follows/routes.js";
 import SectionRoutes from "./sections/routes.js";
 import cors from "cors";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-cs5610-fa23"
+mongoose.connect(CONNECTION_STRING);
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
 
 const app = express();
 app.use(
